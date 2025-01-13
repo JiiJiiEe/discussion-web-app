b9dfe637738197482992d749cecb470388f3ed73
JiiJiiEe <joel.hamalainen359@gmail.com>
2025-01-13 17:46:00 +0200

    models

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,16 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Thread(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
+    #def __str__(self):
+    #    return self.title
 
 class Post(models.Model):
     content = models.TextField()
     thread = models.ForeignKey(Thread, related_name="posts", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.content[:50]
+    #def __str__(self):
+    #    return self.content[:50]
